9fb5794f488b736e054d42946fb3804dc694e3e0
mamboedwin <edwinmambo33@gmail.com>
2020-04-14 15:30:48 +0300

    the other challenges

--- src/estimator.py
+++ src/estimator.py
@@ -21,29 +21,31 @@ def estimator(data):
 def solve(data, multiplier):
     days = nomilise(data['timeToElapse'], data['periodType'])
     reportedCases = int(data['reportedCases'])
-    # totalHospitalBeds = int(data['totalHospitalBeds'])
-    # avgDailyIncomePopulation = float(
-    #     data['region']['avgDailyIncomePopulation'])
-    # avgDailyIncomeInUSD = float(data['region']['avgDailyIncomeInUSD'])
+    totalHospitalBeds = int(data['totalHospitalBeds'])
+    avgDailyIncomePopulation = float(
+        data['region']['avgDailyIncomePopulation'])
+    avgDailyIncomeInUSD = float(data['region']['avgDailyIncomeInUSD'])
     factor = int(days / 3)
     currentlyInfected = reportedCases * int(multiplier)
     infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
 
-    # severeCasesByRequestedTime = int(infectionsByRequestedTime * 0.15)
-    # hospitalBedsByRequestedTime = int(
-    #     (totalHospitalBeds * 0.35) - severeCasesByRequestedTime)
+    severeCasesByRequestedTime = int(infectionsByRequestedTime * 0.15)
+    hospitalBedsByRequestedTime = int(
+        (totalHospitalBeds * 0.35) - severeCasesByRequestedTime)
 
-    # casesForICUByRequestedTime = int(infectionsByRequestedTime * 0.05)
-    # casesForVentilatorsByRequestedTime = int(infectionsByRequestedTime * 0.02)
-    # dollarsInFlight = int((infectionsByRequestedTime *
-    #                        avgDailyIncomePopulation * avgDailyIncomeInUSD) / days)
+    casesForICUByRequestedTime = int(infectionsByRequestedTime * 0.05)
+    casesForVentilatorsByRequestedTime = int(infectionsByRequestedTime * 0.02)
+    dollarsInFlight = int((infectionsByRequestedTime *
+                           avgDailyIncomePopulation * avgDailyIncomeInUSD) / days)
 
     return {"currentlyInfected": currentlyInfected,
-            "infectionsByRequestedTime": infectionsByRequestedTime
-            # "severeCasesByRequestedTime": severeCasesByRequestedTime,
-            # "hospitalBedsByRequestedTime": hospitalBedsByRequestedTime,
-            # "casesForICUByRequestedTime": casesForICUByRequestedTime,
-            # "casesForVentilatorsByRequestedTime": casesForVentilatorsByRequestedTime,
-            # "dollarsInFlight": dollarsInFlight
+            "infectionsByRequestedTime": infectionsByRequestedTime,
+            "severeCasesByRequestedTime": severeCasesByRequestedTime,
+            "hospitalBedsByRequestedTime": hospitalBedsByRequestedTime,
+            "casesForICUByRequestedTime": casesForICUByRequestedTime,
+            "casesForVentilatorsByRequestedTime": casesForVentilatorsByRequestedTime,
+            "dollarsInFlight": dollarsInFlight
             }
 
+from data import data2
+print(estimator(data2))
