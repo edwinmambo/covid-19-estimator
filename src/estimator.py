d317e28faa8f263cace2324f085ff294713e1926
mamboedwin <edwinmambo33@gmail.com>
2020-04-14 15:21:06 +0300

    modified my estimator

--- src/estimator.py
+++ src/estimator.py
@@ -1,42 +1,49 @@
-from data import data1
+# convert period type
+def nomilise(timeToElapse, periodType):
+    period = 1
+    if periodType == "days":
+        period = timeToElapse
+    elif periodType == "weeks":
+        period = int(timeToElapse * 7)
+    elif periodType == "months":
+        period = int(timeToElapse * 30)
+    return int(period)
 
 
-def covid19ImpactEstimator(data):
+def estimator(data):
 
-    impact = {}
-    severeImpact = {}
-    estimate = {
-        "impact": impact,
-        "severeImpact": severeImpact
-    }
+    impact = solve(data, 10)
+    severeImpact = solve(data, 50)
 
-    factor = 3
-    period = data['timeToElapse']
-    reportedCases = data['reportedCases']
-    periodType = data['periodType']
-
-    if periodType == 'days':
-        val = 1
-    elif periodType == 'weeks':
-        val = 7
-    elif periodType == 'months':
-        val = 30
-
-    impact['currentlyInfected'] = data['reportedCases'] * 10
-    severeImpact['currentlyInfected'] = data['reportedCases'] * 50
-
-    impact['infectionsByRequestedTime'] = impact['currentlyInfected'] * \
-        (2 ** ((val * period) // factor))
-    severeImpact['infectionsByRequestedTime'] = severeImpact['currentlyInfected'] * \
-        (2 ** ((val * period) // factor))
-
-    output1 = {
-        "data": data,
-        "impact": impact,
-        "severeImpact": severeImpact
-    }
-
-    return output1
+    return {"data": data, "impact": impact, "severeImpact": severeImpact}
 
 
-covid19ImpactEstimator(data1)
+def solve(data, multiplier):
+    days = nomilise(data['timeToElapse'], data['periodType'])
+    reportedCases = int(data['reportedCases'])
+    # totalHospitalBeds = int(data['totalHospitalBeds'])
+    # avgDailyIncomePopulation = float(
+    #     data['region']['avgDailyIncomePopulation'])
+    # avgDailyIncomeInUSD = float(data['region']['avgDailyIncomeInUSD'])
+    factor = int(days / 3)
+    currentlyInfected = reportedCases * int(multiplier)
+    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+
+    # severeCasesByRequestedTime = int(infectionsByRequestedTime * 0.15)
+    # hospitalBedsByRequestedTime = int(
+    #     (totalHospitalBeds * 0.35) - severeCasesByRequestedTime)
+
+    # casesForICUByRequestedTime = int(infectionsByRequestedTime * 0.05)
+    # casesForVentilatorsByRequestedTime = int(infectionsByRequestedTime * 0.02)
+    # dollarsInFlight = int((infectionsByRequestedTime *
+    #                        avgDailyIncomePopulation * avgDailyIncomeInUSD) / days)
+
+    return {"currentlyInfected": currentlyInfected,
+            "infectionsByRequestedTime": infectionsByRequestedTime
+            # "severeCasesByRequestedTime": severeCasesByRequestedTime,
+            # "hospitalBedsByRequestedTime": hospitalBedsByRequestedTime,
+            # "casesForICUByRequestedTime": casesForICUByRequestedTime,
+            # "casesForVentilatorsByRequestedTime": casesForVentilatorsByRequestedTime,
+            # "dollarsInFlight": dollarsInFlight
+            }
+
